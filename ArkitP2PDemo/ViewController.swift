1ccd55cc5f48358cf09ebf69617b79486e322280
madhuvandhana <madhu.vijay@idt.net>
2020-08-10 21:56:54 -0400

    Initial Commit

---
+++ ArkitP2PDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArkitP2PDemo
+//
+//  Created by Madhu Vijay on 8/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ArkitP2PDemoTests/ArkitP2PDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  ArkitP2PDemoTests.swift
+//  ArkitP2PDemoTests
+//
+//  Created by Madhu Vijay on 8/10/20.
+//
+
+import XCTest
+@testable import ArkitP2PDemo
+
+class ArkitP2PDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
